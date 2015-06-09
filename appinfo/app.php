e9a0dbe64977909065a6b4aa40ab15505b96e5c5
Renaud Fortier <Renaud.Fortier@fsaa.ulaval.ca>
2015-06-09 14:45:30 -0400

    Clean up unnecessary code

--- appinfo/app.php
+++ appinfo/app.php
@@ -11,31 +11,10 @@
 
 namespace OCA\FirstRunWizard\AppInfo;
 
-//\OCP\App::addNavigationEntry([
-//	// the string under which your app will be referenced in owncloud
-//	'id' => 'firstrunwizard',
-//	
-//	// sorting weight for the navigation. The higher the number, the higher
-//	// will it be listed in the navigation
-//	'order' => 10,
-//
-//	// the route that will be shown on startup
-//	'href' => \OCP\Util::linkToRoute('firstrunwizard.page.index'),
-//
-//	// the icon that will be shown in the navigation
-//	// this file needs to exist in img/
-//	'icon' => \OCP\Util::imagePath('firstrunwizard', 'app.svg'),
-//	
-//	// the title of your application. This will be used in the
-//	// navigation or on the settings page of your app
-//	'name' => \OC_L10N::get('firstrunwizard')->t('First run Wizard')
-//]);
-
 if (\OCP\User::isLoggedIn()) {
 	$config = \OC::$server->getConfig();
 	$user = \OC::$server->getUserSession()->getUser();
 
-	$config->setUserValue($user->getUID() , 'firstrunwizard' , 'show' , '1' );
 	if ($config->getUserValue($user->getUID() , 'firstrunwizard' , 'show' , '1' ) === '1'){
 		header('Location: ' . \OC::$server->getURLGenerator()->linkToRoute('firstrunwizard.page.index'));
 	}
