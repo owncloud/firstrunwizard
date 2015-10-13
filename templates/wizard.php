565a71efa0d949b7b4e694979c2d74df6b11bb37
Renaud Fortier <Renaud.Fortier@fsaa.ulaval.ca>
2015-10-13 15:20:08 -0400

    Remove use of deprecated method

--- templates/wizard.php
+++ templates/wizard.php
@@ -1,7 +1,7 @@
 <div id="firstrunwizard">
 
 <a id="closeWizard" class="close">
-	<img class="svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'actions/close.svg')); ?>">
+	<img class="svg" src="<?php print_unescaped(image_path('core', 'actions/close.svg')); ?>">
 </a>
 <h1><?php p($l->t('A safe home for all your data')); ?></h1>
 <?php if ($_['edition'] === ''): ?>
@@ -12,15 +12,15 @@
 
 <h2><?php p($l->t('Get the apps to sync your files'));?></h2>
 <a target="_blank" href="<?php p($_['clients']['desktop']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'desktopapp.svg')); ?>"
+	<img src="<?php print_unescaped(image_path('core', 'desktopapp.svg')); ?>"
 		alt="<?php p($l->t('Desktop client'));?>" />
 </a>
 <a target="_blank" href="<?php p($_['clients']['android']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'googleplay.svg')); ?>"
+	<img src="<?php print_unescaped(image_path('core', 'googleplay.svg')); ?>"
 		alt="<?php p($l->t('Android app'));?>" style="height:60px"/>
 </a>
 <a target="_blank" href="<?php p($_['clients']['ios']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'appstore.svg')); ?>"
+	<img src="<?php print_unescaped(image_path('core', 'appstore.svg')); ?>"
 		alt="<?php p($l->t('iOS app'));?>" style="height:60px"/>
 </a>
 
@@ -28,23 +28,23 @@
 <h2><?php p($l->t('Connect your desktop apps to %s', array($theme->getName()))); ?></h2>
 <a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-calendars')) ?>">
 	<img class="appsmall appsmall-calendar svg" alt=""
-		src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/calendar-dark.svg')); ?>" />
+		src="<?php print_unescaped(image_path('core', 'places/calendar-dark.svg')); ?>" />
 	<?php p($l->t('Connect your Calendar'));?>
 </a>
 <a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-contacts')) ?>">
 	<img class="appsmall appsmall-contacts svg" alt=""
-		src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/contacts-dark.svg')); ?>" />
+		src="<?php print_unescaped(image_path('core', 'places/contacts-dark.svg')); ?>" />
 	<?php p($l->t('Connect your Contacts'));?>
 </a>
 <?php else: ?>
 <br><br><br>
 <a target="_blank" class="button" href="<?php p(link_to_docs('user-manual')); ?>">
-	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('settings', 'help.svg')); ?>" /> <?php p($l->t('Documentation'));?>
+	<img class="appsmall svg" src="<?php print_unescaped(image_path('settings', 'help.svg')); ?>" /> <?php p($l->t('Documentation'));?>
 </a>
 <?php endif; ?>
 <a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>">
 	<img class="appsmall svg" alt=""
-		src="<?php print_unescaped(OCP\Util::imagePath('files', 'folder.svg')); ?>" />
+		src="<?php print_unescaped(image_path('files', 'folder.svg')); ?>" />
 	<?php p($l->t('Access files via WebDAV'));?>
 </a>
 
