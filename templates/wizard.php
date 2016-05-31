20dc7dc7c518c91abc2ed00b922f0ef006c65da4
Michael Stingl <mail@michaelstingl.com>
2016-05-31 11:21:45 +0200

    make .footnote themable for EE
    hardcoded "ownCloud Inc." don't work

--- templates/wizard.php
+++ templates/wizard.php
@@ -59,7 +59,7 @@
 	and <a href="http://owncloud.org/promote"
 		target="_blank">spread the word</a>!')); ?>
 <?php else: ?>
-© 2011-<?php p(date('Y')) ?> <a href="https://owncloud.com" target="_blank">ownCloud Inc.</a>
+© <?php p(date('Y')) ?> <a href="<?php print_unescaped($theme->getBaseUrl()); ?>" target="_blank"><?php print_unescaped($theme->getEntity()); ?></a>
 <?php endif; ?>
 </p>
 
