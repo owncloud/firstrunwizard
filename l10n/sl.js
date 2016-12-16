32e652793aa4ccf83e559f0e979fef14e463f8b7
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-12-17 00:24:16 +0100

    [tx-robot] updated from transifex

--- l10n/sl.js
+++ l10n/sl.js
@@ -11,7 +11,7 @@ OC.L10N.register(
     "Connect your Calendar" : "Poveži koledar",
     "Connect your Contacts" : "Poveži stike",
     "Documentation" : "Dokumentacija",
-    "Access files via WebDAV" : "Dostop do datotek preko WebDAV",
+    "Access files via WebDAV" : "Dostop do datotek prek WebDAV",
     "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Več podrobnosti je zapisanih v <a target=\"_blank\" href=\"%s\">dokumentaciji</a> in na <a target=\"_blank\" href=\"http://owncloud.org\">spletni strani</a>.",
     "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "V kolikor vam je oblak ownCloud všeč,\n\t<a href=\"mailto:?subject=oblak ownCloud\n\t\t&body=Oblak ownCloud je odlična programska oprema za usklajevanje in\n\t\tsouporabo datotek. Na voljo je na http://owncloud.org\">\n\t\tga lahko priporočite prijateljem</a>\n\tin <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">širite dobro besedo</a>!"
 },
