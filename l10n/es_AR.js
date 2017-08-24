60374ee4f853ed74278f99ff6f521296f6c45bc0
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-24 02:32:19 +0000

    [tx-robot] updated from transifex

--- l10n/es_AR.js
+++ l10n/es_AR.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "firstrunwizard",
     {
     "A safe home for all your data" : "Un lugar seguro para todos sus datos",
+    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "Acceder y compartir tus archivos, calendarios, contactos, correo y más desde cualquier dispositivo que tengas",
     "Get the apps to sync your files" : "Obtené Apps para sincronizar tus archivos",
     "Desktop client" : "Cliente de escritorio",
     "Android app" : "App para Android",
@@ -11,6 +12,7 @@ OC.L10N.register(
     "Connect your Contacts" : "Conectá tus Contactos",
     "Documentation" : "Documentación",
     "Access files via WebDAV" : "Acceso a archivos vía WebDAV",
-    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Mas información en la  <a target=\"_blank\" href=\"%s\">documentación</a> y en nuestra <a target=\"_blank\" href=\"http://owncloud.org\">página web</a>."
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Mas información en la  <a target=\"_blank\" href=\"%s\">documentación</a> y en nuestra <a target=\"_blank\" href=\"http://owncloud.org\">página web</a>.",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Si te gusta ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecomiéndalo a tus amigos</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">difunde la palabra</a>!"
 },
 "nplurals=2; plural=(n != 1);");
