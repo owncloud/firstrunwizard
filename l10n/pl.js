e309ad847f4bb35eef8a1590c5449e16842054d8
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-26 02:32:19 +0000

    [tx-robot] updated from transifex

--- l10n/pl.js
+++ l10n/pl.js
@@ -11,6 +11,7 @@ OC.L10N.register(
     "Connect your Contacts" : "Podłącz swoje Kontakty",
     "Documentation" : "Dokumentacja",
     "Access files via WebDAV" : "Uzyskaj dostęp do plików poprzez WebDAV",
-    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Więcej informacji znajduje się w <a target=\"_blank\" href=\"%s\">dokumentacji</a> i na naszej <a target=\"_blank\" href=\"http://owncloud.org\">stronie</a>."
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Więcej informacji znajduje się w <a target=\"_blank\" href=\"%s\">dokumentacji</a> i na naszej <a target=\"_blank\" href=\"http://owncloud.org\">stronie</a>.",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Jeśli podoba Ci się ownCloud,\n<a href=\"mailto:?subject=ownCloud\n&body=ownCloud to wspaniałe otwarte oprogramowanie umożliwiające synchronizację i udostępnianie Twoich plików. \nMożna je pobrać za darmo ze strony http://owncloud.org\">\npoleć ją znajomym</a>\n i <a href=\"http://owncloud.org/promote\"\ntarget=\"_blank\">podziel się informacją</a>!"
 },
 "nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
