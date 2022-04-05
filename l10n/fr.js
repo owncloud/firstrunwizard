8b4bff34241f2f7150bf2361a521e169f8bf3b4a
ownClouders <devops@owncloud.com>
2022-04-05 00:42:36 +0000

    [tx] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -15,4 +15,4 @@ OC.L10N.register(
     "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Des informations complémentaires sont disponibles dans <a target=\"_blank\" href=\"%s\">la documentation</a> et sur <a target=\"_blank\" href=\"http://owncloud.org\">notre site web</a>.",
     "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Si vous aimez ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud est un superbe logiciel libre pour synchroniser et partager vos fichiers. \n\t\tVous pouvez l'obtenir gratuitement sur http://owncloud.org\">\n\t\trecommandez-le à vos amis</a>\n\tet <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">faites passer le mot</a> !"
 },
-"nplurals=2; plural=(n > 1);");
+"nplurals=3; plural=(n==0 || n==1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
