7ca26bada694d70e4f67d9efd50c39c37740b57c
ownClouders <devops@owncloud.com>
2024-03-15 00:07:15 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -15,4 +15,4 @@ OC.L10N.register(
     "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "קיים עוד מידע <a target=\"_blank\" href=\"%s\">בתיעוד</a> כמו כן <a target=\"_blank\" href=\"http://owncloud.org\">באתר</a>.",
     "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "אם ownCloud אהוב עליך,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud הנה תוכנת קוד פתוח לסנכרון ושיתוף של הקבצים שלך.\n\t\tניתן להוריד אותה מ- http://owncloud.org\">\n\t\tכדאי להמליץ לחברים</a>\n\tוכן <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">כדאי להפיץ את המילה</a>!"
 },
-"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
