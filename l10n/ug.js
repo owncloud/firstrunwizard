bd87957cb72e99a33c1fe597f40dcc40eabb47c1
ownClouders <devops@owncloud.com>
2024-10-14 01:40:01 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "firstrunwizard",
+    {
+    "A safe home for all your data" : "بارلىق سانلىق مەلۇماتلىرىڭىز ئۈچۈن بىخەتەر ئۆي",
+    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "ھۆججەتلىرىڭىز ، كالېندارىڭىز ، ئالاقىداشلىرىڭىز ، خەتلىرىڭىز ۋە باشقا ئۈسكۈنىلەردىن شەرتلىرىڭىزنى زىيارەت قىلىڭ",
+    "Get the apps to sync your files" : "ھۆججەتلەرنى قەدەمداشلاش ئۈچۈن ئەپلەرگە ئېرىشىش",
+    "Desktop client" : "ئۈستەلئۈستى خېرىدارى",
+    "Android app" : "ئاندىرويىد ئەپ",
+    "iOS app" : "iOS ئەپ",
+    "Connect your desktop apps to %s" : "ئۈستەل يۈزى ئەپلىرىنى% s غا ئۇلاڭ",
+    "Connect your Calendar" : "يىلنامەڭىزگە باغلاڭ",
+    "Connect your Contacts" : "ئالاقەداشلىرىڭىزغا باغلاڭ",
+    "Documentation" : "قوللانما",
+    "Access files via WebDAV" : "WebDAV ئارقىلىق ھۆججەتلەرنى زىيارەت قىلىڭ",
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "<A target = \"_ blank\" href = \"% s\"> ھۆججەتلەر </a> ۋە ​​بىزنىڭ <a target = \"_ blank\" href = \"http://owncloud.org\"> تور بېتىدە تېخىمۇ كۆپ ئۇچۇرلار بار. >.",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "ئەگەر ئۆزىڭىزنىڭ Cloud نى ياقتۇرسىڭىز ،\n\t<a href = \"mailto :? subject = ownCloud\n\t\t& body = ownCloud ھۆججەتلىرىڭىزنى ماسقەدەملەش ۋە ئورتاقلىشىش ئۈچۈن ناھايىتى ياخشى ئوچۇق يۇمشاق دېتال.\n\t\tئۇنى http://owncloud.org \"> دىن ھەقسىز ئالالايسىز\n\t\tدوستلىرىڭىزغا تەۋسىيە قىلىڭ </a>\n\tۋە <a href = \"http://owncloud.org/promote\"\n\t\ttarget = \"_ blank\"> سۆزنى تارقىتىڭ </a>!"
+},
+"nplurals=2; plural=(n != 1);");
