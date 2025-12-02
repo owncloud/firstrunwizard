e8cc78a867f25e9b68bdb8eef50efae2234bcef0
ownClouders <devops@owncloud.com>
2025-12-02 00:05:26 +0000

    [tx] updated from transifex

---
+++ l10n/zh_HK.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "firstrunwizard",
+    {
+    "A safe home for all your data" : "为您的所有数据提供一个安全的家。",
+    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "在任何设备上访问和共享您的文件，日历，联系人，邮件等等，一切尽在掌控中",
+    "Get the apps to sync your files" : "安装应用进行文件同步",
+    "Desktop client" : "桌面客戶端",
+    "Android app" : "Android 應用程式",
+    "iOS app" : "iOS 應用程式",
+    "Connect your desktop apps to %s" : "連接您的桌面應用程式到 %s",
+    "Connect your Calendar" : "連接您的日曆",
+    "Connect your Contacts" : "連接您的聯絡人",
+    "Documentation" : "文件",
+    "Access files via WebDAV" : "透過 WebDAV 存取檔案",
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "可以從我們的<a target=\"_blank\" href=\"%s\">說明文件</a>和<a target=\"_blank\" href=\"http://owncloud.org\">網站</a>取得更多資訊",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "如果喜欢 ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\t推荐给其他朋友</a>\n\t并 <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">帮助宣传</a>!"
+},
+"nplurals=1; plural=0;");
